2d19f7bb41ec172337a3d1fd98a1105c80dbbdc6
Ivan Medrano <imedrano_@ucol.mx>
2020-02-21 16:32:06 -0600

    Se añadio la base del modulo de pedido

--- pedido.js
+++ pedido.js
@@ -1,10 +1,44 @@
+import Fecha from "./fecha.js";
+import Tiempo from "./tiempo.js";
+import Cliente from "./cliente.js";
+import ElementoPedido from "./elemento-pedido.js"
 
-
-class Pedido {
+export default class Pedido {
+    /**
+     * 
+     * @param {Fecha} fecha 
+     * @param {Tiempo} hora 
+     * @param {Cliente} cliente 
+     * @param {ElementoPedido} elementosPedidos 
+     */
     constructor(fecha, hora, cliente, elementosPedidos){
         this.fecha = fecha;
         this.hora = hora;
         this.cliente = cliente;
         this.elementosPedidos = elementosPedidos;
+    }
+
+    getResumen(){
+
+    }
+
+    getNumeroElementos(){
+
+    }
+
+    getNumeroProductos(){
+
+    }
+
+    getCostoTotal(){
+
+    }
+
+    agregarElemento(){
+
+    }
+
+    listarElementos(){
+
     }
 }
